c382e862850ef2980d7833e2455b9b3861c6013c
Aleksandr_Cherenev <a.cherenev@mail.ru>
2018-04-06 18:57:01 +0400

    Stackoverflow api

--- src/components/SearchQuestions/index.js
+++ src/components/SearchQuestions/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Field } from 'redux-form';
 import { withStyles } from 'material-ui/styles';
 import Button from 'material-ui/Button';
 import styles from './styles';
@@ -8,6 +9,7 @@ const Search = ({
   classes
 }) => (
   <div className={classes.container}>
+    <Field name="pagesize" />
     <input type="text" className={classes.searchField} />
     <Button
       variant="raised"
